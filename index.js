abfd02ab9c2266bd35d240c64ea5b3f7e38c9be0
Ariana Magali Ariza <arianamagaliariza@gmail.com>
2024-07-17 13:54:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('gmailButton').addEventListener('click', function () {
+    window.open('mailto:arianamagaliariza@gmail.com', '_blank');
+});
+
+document.getElementById('githubButton').addEventListener('click', function () {
+    window.open('https://github.com/ArianaMagaliAriza', '_blank');
+});
+
+document.getElementById('linkedinButton').addEventListener('click', function () {
+    window.open('https://www.linkedin.com/in/ariana-magali-ariza-632337313/', '_blank');
+});
+
+document.getElementById('goodreadsButton').addEventListener('click', function () {
+    window.open('https://www.goodreads.com/user/show/90541748-ari', '_blank');
+});
